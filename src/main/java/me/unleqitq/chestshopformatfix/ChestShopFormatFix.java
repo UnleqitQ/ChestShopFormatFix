82ba20ad588ae81b07c2caceac4d0671090ef4ff
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-08-01 23:34:48 +0200

    Initial commit

---
+++ src/main/java/me/unleqitq/chestshopformatfix/ChestShopFormatFix.java
@@ -0,0 +1,18 @@
+package me.unleqitq.chestshopformatfix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChestShopFormatFix extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+	
+}
