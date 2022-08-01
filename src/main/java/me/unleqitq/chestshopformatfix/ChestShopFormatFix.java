e620481a89e25f389e482dc796208a2253f8c47c
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-08-01 23:47:10 +0200

    Update

--- src/main/java/me/unleqitq/chestshopformatfix/ChestShopFormatFix.java
+++ src/main/java/me/unleqitq/chestshopformatfix/ChestShopFormatFix.java
@@ -1,18 +1,26 @@
 package me.unleqitq.chestshopformatfix;
 
+import com.Acrobot.ChestShop.Events.Economy.CurrencyFormatEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ChestShopFormatFix extends JavaPlugin {
+public final class ChestShopFormatFix extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
-		
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	
 	@Override
 	public void onDisable() {
 		// Plugin shutdown logic
+	}
+	
+	@EventHandler
+	public void onFormatRequest(CurrencyFormatEvent event) {
+		event.setFormattedAmount(String.format("%.02f", event.getAmount().doubleValue()));
 	}
 	
 }
